5a73d720618cf352f01fdc7af86b98d835d684ae
prince <phiphat.s@jasmine.com>
2022-09-21 15:00:03 +0700

    test call getserverside but not call axios

--- src/pages/user-management/index.tsx
+++ src/pages/user-management/index.tsx
@@ -1,24 +1,25 @@
 // import { AppConfig } from '@/AppConfig'
-// import { configProduct } from '@/components/lov/config'
-// import { InferGetServerSidePropsType } from 'next';
+import { configProduct } from '@/components/lov/config'
+import { InferGetServerSidePropsType } from 'next'
 // import axiosConfig from '@/util/axiosConfig'
 
 // const { apiUrl } = AppConfig
 
-// export const getServerSideProps = async () => {
+export const getServerSideProps = async () => {
 
-//   const listData = await axiosConfig.get(`${apiUrl}/product/service/product`)
-//   const data = listData.data
-//   const config = {
-//     ...configProduct,
-//     action: { del: {url: String(`${apiUrl}/product/service/product`)}} 
-//   }
-//   return { props: { listData: data, config } }
-// }
+  // const listData = await axiosConfig.get(`${apiUrl}/product/service/product`)
+  const listData = {a: 'prince'}
+  const data = listData.a
+  const config = {
+    ...configProduct, 
+    // action: { del: {url: String(`${apiUrl}/product/service/product`)}} 
+  }
+  return { props: { listData: data, config } }
+}
 
 // const User = ({listData, config}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-const User = () => {
-
+const User = ({listData, config}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(listData, config)
   return (
     <>
       Manage user
