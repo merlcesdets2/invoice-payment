086d58edd46b0f8fef6d399f2305ec51ecbec5a6
prince <phiphat.s@jasmine.com>
2022-09-21 16:36:17 +0700

    try another api

--- src/pages/user-management/index.tsx
+++ src/pages/user-management/index.tsx
@@ -1,16 +1,18 @@
-import { AppConfig } from '@/AppConfig'
+// import { AppConfig } from '@/AppConfig'
 import { configProduct } from '@/components/lov/config'
 import { InferGetServerSidePropsType } from 'next'
 // import axiosConfig from '@/util/axiosConfig'
 
-const { apiUrl } = AppConfig
+// const { apiUrl } = AppConfig
 
 export const getServerSideProps = async () => {
 
-  const listData2 = await fetch(`${apiUrl}/product/service/product`, { headers: {
-    'accessToken': 'invoiceToken',
-  }})
-  console.log(await listData2.json())
+  const listData2 = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  // const listData2 = await fetch(`${apiUrl}/product/service/product`, { headers: {
+  //   'accessToken': 'invoiceToken',
+  // }})
+  const a = await listData2.json()
+  console.log(a.name)
   const listData = {a: 'prince'}
   const data = listData.a
   const config = {
