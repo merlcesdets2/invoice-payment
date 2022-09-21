0f25556efc770c93a3306806bb36d9503e675dc3
prince <phiphat.s@jasmine.com>
2022-09-21 14:45:48 +0700

    test remove call api on get serverside cannot deploy

--- src/pages/user-management/index.tsx
+++ src/pages/user-management/index.tsx
@@ -1,23 +1,24 @@
-import { AppConfig } from '@/AppConfig'
-import { configProduct } from '@/components/lov/config'
-import { InferGetServerSidePropsType } from 'next';
-import axiosConfig from '@/util/axiosConfig'
+// import { AppConfig } from '@/AppConfig'
+// import { configProduct } from '@/components/lov/config'
+// import { InferGetServerSidePropsType } from 'next';
+// import axiosConfig from '@/util/axiosConfig'
 
-const { apiUrl } = AppConfig
+// const { apiUrl } = AppConfig
 
-export const getServerSideProps = async () => {
+// export const getServerSideProps = async () => {
 
-  const listData = await axiosConfig.get(`${apiUrl}/product/service/product`)
-  const data = listData.data
-  const config = {
-    ...configProduct,
-    action: { del: {url: String(`${apiUrl}/product/service/product`)}} 
-  }
-  return { props: { listData: data, config } }
-}
+//   const listData = await axiosConfig.get(`${apiUrl}/product/service/product`)
+//   const data = listData.data
+//   const config = {
+//     ...configProduct,
+//     action: { del: {url: String(`${apiUrl}/product/service/product`)}} 
+//   }
+//   return { props: { listData: data, config } }
+// }
 
-const User = ({listData, config}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  console.log(listData, config)
+// const User = ({listData, config}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const User = () => {
+
   return (
     <>
       Manage user
